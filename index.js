670c58c9fbd76b4094f6c8c134419f1d5d669f96
Wtm <micromaomao@gmail.com>
2016-01-30 01:34:09 +0800

    img.maowtm.org: resize-able (?width=200)

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.N_DB);
 const db = mongoose.connection;
+var redisClient = require("redis").createClient({
+    host: '127.6.0.233'
+});
+global.lock = require("redis-lock")(redisClient);
 db.on('error', function (err) {
     console.error(err);
     console.error('Faild to connect to db. Exiting...');
@@ -23,10 +27,12 @@ db.on('open', function() {
             next();
         }
     });
+
+    app.use(require('./subs/static'));
+
     app.use(function(req, res, next) {
         var prasedUrl = url.parse(req.originalUrl, false);
-        if(req.method == 'GET' && prasedUrl.pathname.substr(-1) != '/'
-            && req.hostname != 'static.maowtm.org') {
+        if(req.method == 'GET' && prasedUrl.pathname.substr(-1) != '/') {
             prasedUrl.pathname += '/';
             res.redirect(302, url.format(prasedUrl));
         } else {
@@ -35,28 +41,52 @@ db.on('open', function() {
     });
 
     app.use(require('./subs/main'));
-    app.use(require('./subs/static'));
 
-    http.createServer(app).listen(80, process.env.N_LISTEN);
-    const httpsopts = {
-        key: fs.readFileSync(process.env.N_SSLKEY),
-        cert: fs.readFileSync(process.env.N_SSLCERT),
-        ciphers: [
-            "ECDHE-RSA-AES256-SHA384",
-            "DHE-RSA-AES256-SHA384",
-            "ECDHE-RSA-AES256-SHA256",
-            "DHE-RSA-AES256-SHA256",
-            "ECDHE-RSA-AES128-SHA256",
-            "DHE-RSA-AES128-SHA256",
-            "HIGH",
-            "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!SRP", "!CAMELLIA"
-        ].join(':'),
-        honorCipherOrder: true
-    };
-    https.createServer(httpsopts, app).listen(443, process.env.N_LISTEN);
-    if(process.env.N_LISTEN2) {
-        http.createServer(app).listen(80, process.env.N_LISTEN2);
-        https.createServer(httpsopts, app).listen(443, process.env.N_LISTEN2);
+    var image = mongoose.model('image');
+    var imgs = [{
+        name: "avatar.png",
+        path: "static/imgs/avatar.png"
+    }, {
+        name: "bitcoin-mainaddr-qr.png",
+        path: "static/imgs/bitcoin-mainaddr-qr.png"
+    }, {
+        name: "maze.png",
+        path: "static/imgs/maze.png"
+    }];
+    function doAddImg(i) {
+        if (i >= imgs.length) {
+            http.createServer(app).listen(80, process.env.N_LISTEN);
+            const httpsopts = {
+                key: fs.readFileSync(process.env.N_SSLKEY),
+                cert: fs.readFileSync(process.env.N_SSLCERT),
+                ciphers: [
+                    "ECDHE-RSA-AES256-SHA384",
+                    "DHE-RSA-AES256-SHA384",
+                    "ECDHE-RSA-AES256-SHA256",
+                    "DHE-RSA-AES256-SHA256",
+                    "ECDHE-RSA-AES128-SHA256",
+                    "DHE-RSA-AES128-SHA256",
+                    "HIGH",
+                    "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!SRP", "!CAMELLIA"
+                ].join(':'),
+                honorCipherOrder: true
+            };
+            https.createServer(httpsopts, app).listen(443, process.env.N_LISTEN);
+            if(process.env.N_LISTEN2) {
+                http.createServer(app).listen(80, process.env.N_LISTEN2);
+                https.createServer(httpsopts, app).listen(443, process.env.N_LISTEN2);
+            }
+        } else {
+            image.addImageIfNotExist(imgs[i].name, __dirname + '/' + imgs[i].path, function (err) {
+                if(err) {
+                    console.error(err);
+                    console.error('Faild to load image: ' + imgs[i].name);
+                    process.exit(1);
+                } else {
+                    doAddImg(i + 1);
+                }
+            });
+        }
     }
-
+    doAddImg(0);
 });
--- subs/static.js
+++ subs/static.js
@@ -1,11 +1,142 @@
 const express = require('express');
 const pages = require('../pages');
+const mongoose = require('mongoose');
+const lwip = require('lwip');
+const qs = require('querystring');
 
-var r_static = express.static('static');
+var imageSchema = new mongoose.Schema({
+    name: 'String',
+    src: 'Buffer',
+    width: 'Number'
+});
+var cachedScaleSchema = new mongoose.Schema({
+    imgId: 'ObjectId',
+    scale: 'Number',
+    data: 'Buffer'
+});
+imageSchema.static('addImageIfNotExist', function (imgname, filepath, callback) {
+    image.findOne({ name: imgname }, function (err, igd) {
+        if(err)
+            callback(err, null);
+        else if (igd)
+            callback();
+        else {
+            lwip.open(filepath, function (err, img) {
+                if(err)
+                    callback(err, null);
+                else {
+                    var imgdoc = new image({
+                        name: imgname,
+                        width: img.width()
+                    });
+                    img.toBuffer('png', function (err, buff) {
+                        if(err) {
+                            callback(err, null);
+                        } else {
+                            imgdoc.set('src', buff);
+                            imgdoc.save(callback);
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+imageSchema.method('queryScale', function (scale, allowEnlarge, callback) {
+    scale = Math.ceil(scale / 50) * 50;
+    if ((scale > this.width && !allowEnlarge) || scale == this.width) {
+        callback(null, this.src);
+        return;
+    }
+    var th = this;
+    global.lock('imageCaching\t' + th._id.toString(), function(done) {
+        cachedScale.findOne({ imgId: th._id, scale: scale }, function (err, doc) {
+            if(err) {
+                callback(err, null);
+                done();
+            } else {
+                if(doc) {
+                    callback(null, doc.data);
+                    done();
+                } else {
+                    var doc = new cachedScale({
+                        imgId: th._id,
+                        scale: scale
+                    });
+                    lwip.open(th.src, 'png', function (err, img) {
+                        if(err) {
+                            callback(err, null);
+                            done();
+                        } else {
+                            var sf = scale / th.width;
+                            img.scale(sf, function (err, nimg) {
+                                if(err) {
+                                    callback(err, null);
+                                    done();
+                                } else {
+                                    nimg.toBuffer('png', function (err, buff) {
+                                        if(err) {
+                                            callback(err, null);
+                                            done();
+                                        } else {
+                                            doc.set('data', buff);
+                                            doc.save(function(err) {
+                                                callback(null, buff);
+                                                done();
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    });
+});
 
-module.exports = function(req, res, next) {
-    if(req.hostname == 'static.maowtm.org') {
+var image = mongoose.model('image', imageSchema);
+var cachedScale = mongoose.model('cachedScale', cachedScaleSchema);
+
+var r_static = express.Router();
+r_static.use(express.static('static'));
+var r_img = express.Router({
+    strict: true
+});
+r_img.get('/:imgname', function (req, res, next) {
+    var scale = parseInt(req.query.width);
+    if(req.query.width && (scale.toString() != req.query.width || scale <= 0)) {
+        scale = Infinity;
+    }
+    image.findOne({ name: req.params.imgname }, function (err, img) {
+        if(err)
+            res.error(err);
+        else if (!img) {
+            next();
+        } else {
+            if (scale > img.width) {
+                delete req.query.width;
+                res.redirect(302, req.path + qs.stringify(req.query));
+                return;
+            }
+            img.queryScale(scale || img.width, false, function (err, buff) {
+                if(err)
+                    res.error(err);
+                else {
+                    res.type('png');
+                    res.send(buff);
+                }
+            });
+        }
+    });
+});
+
+module.exports =  function(req, res, next) {
+    if (req.hostname == 'static.maowtm.org') {
         r_static(req, res, next);
+    } else if (req.hostname == 'img.maowtm.org') {
+        r_img(req, res, next);
     } else {
         next();
     }
