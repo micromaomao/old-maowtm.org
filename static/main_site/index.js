6585fec17b9f2dd509334490800746baf1177995
Wtm <micromaomao@gmail.com>
2016-03-05 21:54:51 +0800

    Change index UI

--- static/main_site/index.js
+++ static/main_site/index.js
@@ -10,4 +10,25 @@ $(function () {
             e.preventDefault();
         });
     });
+    var header = $('.header');
+    var headbg = header.find('.background');
+    var bhc = header.find('.big-header-content');
+    var tsy = 250;
+    var bhch;
+    function find_bhch () {
+        bhc.css({"height": ""});
+        bhch = (bhc.length ? bhc.height() : 0)
+        tsy = header.height();
+    }
+    function am () {
+        requestAnimationFrame(am);
+        find_bhch();
+        var t = Math.min(window.scrollY / tsy, 1);
+        headbg.css({opacity: 1 - t});
+        if (bhch > 0) {
+            bhc.css({height: (bhch * (1 - t)) + "px", opacity: 1 - t});
+            bhc.css("pointer-events", (t>=0.5?"none":""));
+        }
+    }
+    am();
 });
