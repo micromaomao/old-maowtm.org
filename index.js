4b2e5ae5762d07de8f0830d6aa8833644cc6f925
Wtm <micromaomao@gmail.com>
2016-06-25 19:25:50 +0800

    Refactor server index.js
    Add HTTP/2 support
    Remove activity stuff
    Remove mail subdomain.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const https = require('https');
 const http = require('http');
+const http2 = require('http2');
 const fs = require('fs');
 const url = require('url');
 const app = express();
@@ -8,6 +8,8 @@ const pages = require('./pages');
 const mongoose = require('mongoose');
 const redis = require("redis");
 const redislock = require("redis-lock");
+
+// This file will be launched with launcher.js.
 
 var maowtm = function (config) {
     config = config || {};
@@ -20,22 +22,16 @@ var maowtm = function (config) {
         host: this._redis
     });
     this.lock = redislock(this.redis);
+    // this._listen can be a array of address.
     this._listen = config.listen || "127.0.0.1";
-    if (typeof this._listen == 'object') {
-        if (this._listen.length > 2) {
-            throw new Error("Can't listen more than 2.");
-        }
-        this._listen2 = this._listen[1];
-        this._listen = this._listen[0];
-    }
     this._ssl = config.ssl;
     if (!(this._ssl && this._ssl.cert && this._ssl.key))
-        throw new Error("No ssl cert provided")
+        throw new Error("No SSL certificate provided")
     var _this = this;
 
     this.db.on('error', function (err) {
         console.error(err);
-        console.error('Faild to connect to db. Exiting...');
+        console.error('Error connecting to db. Exiting...');
         process.exit(1);
     });
     this.db.on('open', function() {
@@ -88,7 +84,6 @@ var maowtm = function (config) {
         });
 
         app.use(require('./subs/main'));
-        app.use(require('./subs/mail'));
         
         app.use(function(req, res) {
             res.error(404, "Not find");
@@ -96,20 +91,35 @@ var maowtm = function (config) {
 
         var image = mongoose.model('image');
         var imgs = fs.readdirSync('static/imgs');
-        var doAddActivities;
-        var activityFiles;
-        var actcd = __dirname + '/data/activities';
+        function doSetupServer() {
+            const httpsopts = {
+                key: fs.readFileSync(_this._ssl.key),
+                cert: fs.readFileSync(_this._ssl.cert),
+                ciphers: ["ECDHE-RSA-AES128-GCM-SHA256", "ECDHE-ECDSA-AES128-GCM-SHA256", "ECDHE-RSA-AES256-GCM-SHA384",
+                    "ECDHE-ECDSA-AES256-GCM-SHA384", "DHE-RSA-AES128-GCM-SHA256", "DHE-DSS-AES128-GCM-SHA256", "kEDH+AESGCM",
+                    "ECDHE-RSA-AES128-SHA256", "ECDHE-ECDSA-AES128-SHA256", "ECDHE-RSA-AES128-SHA", "ECDHE-ECDSA-AES128-SHA",
+                    "ECDHE-RSA-AES256-SHA384", "ECDHE-ECDSA-AES256-SHA384", "ECDHE-RSA-AES256-SHA", "ECDHE-ECDSA-AES256-SHA",
+                    "DHE-RSA-AES128-SHA256", "DHE-RSA-AES128-SHA", "DHE-DSS-AES128-SHA256", "DHE-RSA-AES256-SHA256",
+                    "DHE-DSS-AES256-SHA", "DHE-RSA-AES256-SHA", "ECDHE-RSA-DES-CBC3-SHA", "ECDHE-ECDSA-DES-CBC3-SHA",
+                    "EDH-RSA-DES-CBC3-SHA", "AES128-GCM-SHA256", "AES256-GCM-SHA384", "AES128-SHA256", "AES256-SHA256",
+                    "AES128-SHA", "AES256-SHA", "AES", "CAMELLIA", "DES-CBC3-SHA", "!aNULL", "!eNULL", "!EXPORT", "!DES",
+                    "!RC4", "!MD5", "!PSK", "!aECDH", "!EDH-DSS-DES-CBC3-SHA", "!KRB5-DES-CBC3-SHA"].join(":"),
+                honorCipherOrder: true
+            };
+            if(!Array.isArray(_this._listen)) {
+                http2.createServer(httpsopts, app).listen(443, _this._listen);
+                http.createServer(app).listen(80, _this._listen);
+            } else {
+                _this._listen.forEach(function (address) {
+                    http2.createServer(httpsopts, app).listen(443, address);
+                    http.createServer(app).listen(80, address);
+                });
+            }
+            return;
+        }
         function doAddImg(i) {
             if (i >= imgs.length) {
-                fs.readdir(actcd, function (err, files) {
-                    if(err) {
-                        console.error(err);
-                        process.exit(1);
-                    } else {
-                        activityFiles = files;
-                        doAddActivities(0);
-                    }
-                });
+                doSetupServer();
             } else {
                 image.addImageIfNotExist(imgs[i], __dirname + '/static/imgs/' + imgs[i], function (err) {
                     if(err) {
@@ -121,38 +131,6 @@ var maowtm = function (config) {
                     }
                 });
             }
-        }
-        var activityImportLog = mongoose.model('activityImportLog');
-        doAddActivities = function (i) {
-            if(i >= activityFiles.length) {
-                http.createServer(app).listen(80, _this._listen);
-                const httpsopts = {
-                    key: fs.readFileSync(_this._ssl.key),
-                    cert: fs.readFileSync(_this._ssl.cert),
-                    ciphers: "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA",
-                    honorCipherOrder: true
-                };
-                https.createServer(httpsopts, app).listen(443, _this._listen);
-                if(_this._listen2) {
-                    http.createServer(app).listen(80, _this._listen2);
-                    https.createServer(httpsopts, app).listen(443, _this._listen2);
-                }
-                return;
-            }
-            var fname = activityFiles[i];
-            if(!fname.match(/^[a-zA-Z0-9]+\.json$/)) {
-                doAddActivities(i + 1);
-                return;
-            }
-            activityImportLog.importFile(fname, function (err) {
-                if(err) {
-                    console.error(err);
-                    console.error('Faild to import ' + activityFiles[i] + ' .');
-                    process.exit(1);
-                } else {
-                    doAddActivities(i + 1);
-                }
-            }, actcd);
         }
         doAddImg(0);
     });
