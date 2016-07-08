fa62772cc9f7f0ef41bc8b523bb5cd99a1d308a5
Wtm <micromaomao@gmail.com>
2016-07-08 23:09:17 +0800

    Minify html output and provide source link

--- pages.js
+++ pages.js
@@ -6,11 +6,17 @@ const path = require('path');
 var pages = {};
 var pagesfo = __dirname + '/pages/';
 var list = fs.readdirSync(pagesfo);
+const ghUrl = "https://github.com/micromaomao/maowtm.org/tree/master/";
 function mapStatic(url) {
     url = url.getValue();
     if (url[0] !== "/")
         url = "/" + url;
     return sass.types.String("url(https://static.maowtm.org" + url + ")");
+}
+function preProcess(html, pugFile, sassFile) {
+    return "<!-- Mixed and minified html + css:\n" +
+           "     Source:      " + ghUrl + "pages/" + pugFile + "\n" +
+           "     Style sheet: " + (sassFile ? (ghUrl + "style/" + sassFile) : "none") + " -->\n\n" + html.replace(/\s{0,}\n\s{0,}/g, " ");
 }
 list.forEach(function (fname) {
     var fnmatch = fname.match(/^([A-Za-z0-9\-_]+)\.pug/);
@@ -35,11 +41,13 @@ list.forEach(function (fname) {
                         o = o || {};
                         var n = Object.create(o);
                         n.style = sassResult.css;
-                        return pugfn(n);
+                        return preProcess(pugfn(n), fname, name + '.sass');
                     };
                 });
             } else {
-                pages[name] = pugfn;
+                pages[name] = function(o) {
+                    return preProcess(pugfn(o), fname);
+                };
                 console.log('pages: style for ' + fname + ' not find.');
             }
         });
