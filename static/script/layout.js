cf9d6ca99c71f239148cbc3a70f5aea9bfb87012
Wtm <micromaomao@gmail.com>
2016-08-03 20:28:13 +0800

    Change hash while scrolling

--- static/script/layout.js
+++ static/script/layout.js
@@ -1,4 +1,4 @@
-function checkSize() {
+function check() {
     var mobileClassRegex = /(^|\s)mobile($|\s)/;
     if (window.innerWidth < 580) {
         if (!document.body.className.match(mobileClassRegex)) {
@@ -8,6 +8,29 @@ function checkSize() {
         document.body.className =
             document.body.className.replace(mobileClassRegex, "");
     }
+
+    if (window.pageYOffset <= 50) {
+        changeHash(null);
+    } else {
+        var elearr = document.querySelectorAll('[id]');
+        var currentAnchor = Array.prototype.map.call(elearr, function(e) {
+            return {diffTop: Math.abs(e.getBoundingClientRect().top), e: e};
+        }).sort(function(a, b) {
+            if (a.diffTop < b.diffTop) {
+                return -1;
+            } else {
+                return (a.diffTop > b.diffTop ? 1 : 0);
+            }
+        })[0].e;
+        changeHash(currentAnchor.id);
+    }
 }
-setInterval(checkSize, 100);
-checkSize();
+function changeHash(hash) {
+    var selfUrl = window.location.toString().replace(/#.+$/g, "");
+    if (hash !== null) {
+        selfUrl += "#" + hash;
+    }
+    window.history.replaceState({}, document.title, selfUrl);
+}
+setInterval(check, 100);
+check();
